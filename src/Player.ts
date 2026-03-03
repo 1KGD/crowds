34121f5db200e90bfc858c2924dfe4a88bf2413e
mrmcscreamypant <161328176+mrmcscreamypant@users.noreply.github.com>
2026-03-03 21:15:04 +0000

    full on player

--- src/Player.ts
+++ src/Player.ts
@@ -5,6 +5,8 @@ import { ArenaAssets } from "./scenes/Arena";
 const enum PlayerAnimations {
     WALK_DOWN = "walkDown",
     WALK_UP = 'walkUp',
+    WALK_LEFT = 'walkLeft',
+    WALK_RIGHT = 'walkRight',
 }
 
 const enum PlayerControls {
@@ -23,8 +25,6 @@ export default class Player extends Phaser.GameObjects.Sprite {
 
         this.setupAnims();
         this.keys = this.setupControls();
-
-        this.anims.play(PlayerAnimations.WALK_DOWN);
     }
 
     private setupAnims(): void {
@@ -38,7 +38,9 @@ export default class Player extends Phaser.GameObjects.Sprite {
         };;
 
         walkAnim(PlayerAnimations.WALK_DOWN, { start: 0, end: 3 });
-        walkAnim(PlayerAnimations.WALK_UP, {start: 34, end: 37});
+        walkAnim(PlayerAnimations.WALK_UP, { start: 34, end: 37 });
+        walkAnim(PlayerAnimations.WALK_LEFT, { start: 51, end: 54 });
+        walkAnim(PlayerAnimations.WALK_RIGHT, { start: 17, end: 20 });
     }
 
     private setupControls(): { [key in PlayerControls]: Phaser.Input.Keyboard.Key } {
@@ -51,18 +53,32 @@ export default class Player extends Phaser.GameObjects.Sprite {
 
         const keys = this.scene.input.keyboard?.addKeys(controlMap) as { [key in PlayerControls]: Phaser.Input.Keyboard.Key };
 
-        keys.down.on("down", () => this.anims.play(PlayerAnimations.WALK_DOWN));
-        keys.up.on("down", () => this.anims.play(PlayerAnimations.WALK_UP));
-
         return keys;
     }
 
     public override preUpdate(time: number, delta: number): void {
         super.preUpdate(time, delta);
+        const dir = { x: 0, y: 0 };
 
-        if (this.keys.down.isDown) this.y += delta * 0.1;
-        else if (this.keys.up.isDown) this.y -= delta * 0.1;
-        else if (this.keys.left.isDown) this.x -= delta * 0.1;
-        else if (this.keys.right.isDown) this.x += delta * 0.1;
+        if (this.keys.down.isDown) dir.y += 1;
+        if (this.keys.up.isDown) dir.y -= 1;
+        if (this.keys.left.isDown) dir.x -= 1;
+        if (this.keys.right.isDown) dir.x += 1;
+
+        const dist = Math.sqrt(dir.x ** 2 + dir.y ** 2);
+        if (dist > 0) {
+            const normalized = { x: dir.x / dist, y: dir.y / dist };
+            this.x += normalized.x * delta * 0.1;
+            this.y += normalized.y * delta * 0.1;
+
+            let anim;
+            if (dir.x !== 0) anim = dir.x < 0 ? PlayerAnimations.WALK_LEFT : PlayerAnimations.WALK_RIGHT;
+            else anim = dir.y < 0 ? PlayerAnimations.WALK_UP : PlayerAnimations.WALK_DOWN;
+
+            if (this.anims.getName() !== anim) this.anims.play(anim);
+        } else if (this.anims.isPlaying) {
+            this.anims.restart();
+            this.anims.stop();
+        }
     }
 }
