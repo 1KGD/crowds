a33999dd23a28e2a170d5cb62f59ab99299290a3
mrmcscreamypant <161328176+mrmcscreamypant@users.noreply.github.com>
2026-03-12 15:31:44 +0000

    hexagon

--- src/creature/test_dummy.rs
+++ src/creature/test_dummy.rs
@@ -1,35 +1,38 @@
-use wasm_bindgen::prelude::*;
-use noise::*;
+use std::f32;
+
 use crate::creature::*;
+use noise::*;
+use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 #[derive(Clone)]
 pub struct TestDummy {
     noise: Simplex,
+    heading: f32,
 }
 
 impl TestDummy {
     pub fn new(seed: u32) -> Self {
+        let noise = Simplex::new(seed);
         TestDummy {
-            noise: Simplex::new(seed),
+            noise: noise,
+            heading: noise.get([0.3, 0.6]) as f32 * f32::consts::PI * 2.,
         }
     }
 }
 
 impl CreatureBehavior for TestDummy {
     fn tick(&mut self, delta: f32, world: &World, creature: &mut CreatureProps) {
-        let theta: f32 = f32::floor(
-            (self.noise.get([
-                (creature.pos.x * 96. / world.shape.x_f32()) as f64,
-                (creature.pos.y * 96. / world.shape.y_f32()) as f64,
-            ]) + 0.5) as f32
-                * 4.,
-        ) / 4.
-            * f32::consts::PI
-            * 2.;
+        self.heading += self.noise.get([
+            (creature.pos.x * 96. / world.shape.x_f32()) as f64,
+            (creature.pos.y * 96. / world.shape.y_f32()) as f64,
+        ]) as f32
+            / 16.
+            * delta;
+        let theta = f32::floor(self.heading * 8.) / 8. * f32::consts::PI * 2.;
         creature.pos.x += f32::sin(theta) * 5. * delta;
         creature.pos.y += f32::cos(theta) * 5. * delta;
 
-        creature.anim = theta as u8;
+        creature.anim = (theta % 1.) as u8;
     }
 }
